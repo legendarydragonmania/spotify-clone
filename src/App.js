68b11a26f9ea16a07c68218bb0b6a923e7092710
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-07 10:02:14 +0500

    login nearly ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
+import Login from './Login'
 
 function App() {
-  return <div className='app'></div>
+  return (
+    <div className='app'>
+      <Login />
+
+    </div>
+  )
 }
 
 export default App
